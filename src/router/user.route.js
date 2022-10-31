75f9d7057955529835c7007f7d9aaf9ea28a200b
lay <>
2022-10-31 19:40:14 +0800

    s

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -4,7 +4,6 @@ const { userValidator, verifyUser } = require('../middleware/user.middleware');
 const router = new Router({ prefix: '/user' });
 
 const { register, login } = require('../controller/user.controllse');
-const { getDetail } = require('../controller/lay.controllse');
 
 // 注册接口
 router.post('/register', userValidator, verifyUser, register);
